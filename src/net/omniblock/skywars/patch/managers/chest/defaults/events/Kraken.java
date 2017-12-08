cccfd485bf605fbbec5f5406bacce35bb0833df8
SONY <SONY@DESKTOP-19IFJ4I>
2017-12-08 11:43:02 -0500

    SKYWARS: 123RE-12.0.2 (Cambios en los Items del SkyWars Z: Kraken y poción)

--- src/net/omniblock/skywars/patch/managers/chest/defaults/events/Kraken.java
+++ src/net/omniblock/skywars/patch/managers/chest/defaults/events/Kraken.java
@@ -1,19 +1,19 @@
 package net.omniblock.skywars.patch.managers.chest.defaults.events;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.bukkit.Effect;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Squid;
 import org.bukkit.event.EventHandler;
@@ -24,104 +24,115 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import net.omniblock.network.library.helpers.effectlib.effect.ExplodeEffect;
 import net.omniblock.skywars.Skywars;
+import net.omniblock.skywars.SkywarsGameState;
+import net.omniblock.skywars.games.solo.events.SoloPlayerBattleListener;
+import net.omniblock.skywars.games.solo.events.SoloPlayerBattleListener.DamageCauseZ;
 import net.omniblock.skywars.games.solo.managers.SoloPlayerManager;
+import net.omniblock.skywars.games.teams.events.TeamPlayerBattleListener;
 import net.omniblock.skywars.games.teams.managers.TeamPlayerManager;
-import net.omniblock.skywars.patch.managers.chest.defaults.events.type.ItemType;
+import net.omniblock.skywars.patch.types.SkywarsType;
 import net.omniblock.skywars.util.block.SpawnBlock;
 
-public class Kraken implements Listener, ItemType {
+public class Kraken implements Listener {
 
-	private List<Arrow> getArrow = new ArrayList<Arrow>();
+	private List<Fireball> getFireball = new ArrayList<Fireball>();
 	private List<Block> coalBlock = new ArrayList<Block>();
+	private List<Block> clayBlock = new ArrayList<Block>();
 
-	private Map<Arrow, Squid> getSquid = new HashMap<Arrow, Squid>();
-	private Map<Player, List<Block>> squidLauncher = new HashMap<Player, List<Block>>();
-
-	@SuppressWarnings("deprecation")
+	private Map<Fireball, Squid> getSquid = new HashMap<Fireball, Squid>();
+	private Map<Fireball, Player> getPlayer = new HashMap<Fireball, Player>();
+	
 	@EventHandler
-	@Override
 	public void kraken(PlayerInteractEvent event) {
 
-		Player player = event.getPlayer();
+		if (Skywars.getGameState() != SkywarsGameState.IN_GAME) return;
 
-		if (SoloPlayerManager.getPlayersInGameList().contains(player)
-				|| TeamPlayerManager.getPlayersInGameList().contains(player)
-						&& player.getGameMode() == GameMode.SURVIVAL) {
+		if (SoloPlayerManager.getPlayersInGameList().contains(event.getPlayer())
+				|| TeamPlayerManager.getPlayersInGameList().contains(event.getPlayer())) {
+			
+			if(event.getPlayer().getItemInHand().getType() == Material.RECORD_6) {
+				
+				if (event.getAction() == Action.RIGHT_CLICK_AIR 
+						|| event.getAction() == Action.RIGHT_CLICK_BLOCK
+						|| event.getAction() == Action.LEFT_CLICK_AIR
+						|| event.getAction() == Action.LEFT_CLICK_BLOCK) {
 
-			if (event.getPlayer().getItemInHand().hasItemMeta()) {
-				if (event.getPlayer().getItemInHand().getItemMeta().hasDisplayName()) {
-					if (event.getPlayer().getItemInHand().getType() == Material.RECORD_6) {
-						if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK
-								|| event.getAction() == Action.LEFT_CLICK_AIR
-								|| event.getAction() == Action.LEFT_CLICK_BLOCK) {
+					if (event.getClickedBlock() != null) {
+						if (event.getClickedBlock().getType() == Material.CHEST
+								|| event.getClickedBlock().getType() == Material.TRAPPED_CHEST
+								|| event.getClickedBlock().getType() == Material.JUKEBOX) {
 
-							if (event.getClickedBlock() != null) {
-								if (event.getClickedBlock().getType() == Material.CHEST
-										|| event.getClickedBlock().getType() == Material.TRAPPED_CHEST
-										|| event.getClickedBlock().getType() == Material.JUKEBOX) {
-
-									event.setCancelled(true);
-									return;
-
-								}
-							}
-
-							event.getPlayer().getInventory().setItemInHand(null);
-
-							Squid squid = (Squid) event.getPlayer().getWorld()
-									.spawnEntity(event.getPlayer().getLocation().add(0, 2, 0), EntityType.SQUID);
-							squid.isInvulnerable();
-
-							final Arrow arrow = event.getPlayer().launchProjectile(Arrow.class);
-							Vector dir = event.getPlayer().getLocation().getDirection().normalize().multiply(5);
-
-							getArrow.add(arrow);
-							getSquid.put(arrow, squid);
-							squidLauncher.put(event.getPlayer(), coalBlock);
-							
-							squid.isInvulnerable();
-							squid.setMaxHealth(1000);
-							squid.setHealth(1000);
-							arrow.setPassenger(squid);
-							arrow.setVelocity(dir);
+							event.setCancelled(true);
+							return;
 
 						}
 					}
+					
+					event.getPlayer().getInventory().setItemInHand(null);
+
+					final Fireball fireball = event.getPlayer().launchProjectile(Fireball.class);
+					Vector dir = event.getPlayer().getLocation().getDirection().normalize().multiply(5);
+					
+					Squid squid = (Squid) event.getPlayer().getWorld().spawnEntity(event.getPlayer().getLocation(), EntityType.SQUID);
+					
+					makeIA(squid, fireball, event.getPlayer(), dir);
+					
+					
+					
 				}
 			}
 		}
 	}
 
+	public void makeIA(Squid squid, Fireball fireball, Player player, Vector dir) {
+
+		getFireball.add(fireball);
+		getSquid.put(fireball, squid);
+		getPlayer.put(fireball, player);
+		
+		squid.isInvulnerable();
+		squid.setMaxHealth(1000);
+		squid.setHealth(1000);
+		squid.setGravity(false);
+
+		fireball.setGravity(false);;
+		fireball.setIsIncendiary(false);
+		fireball.setPassenger(squid);
+		fireball.setVelocity(dir);
+
+	}
+
 	@EventHandler
-	public void arrowEffect(ProjectileHitEvent event) {
+	public void hitEvent(ProjectileHitEvent event) {
+		
+		if (event.getEntity() instanceof Fireball) {
 
-		if (event.getEntity() instanceof Arrow) {
+			Fireball fb = (Fireball) event.getEntity();
 
-			Arrow aw = (Arrow) event.getEntity();
-
-			if (aw == null) {
+			if (fb == event.getHitEntity())
 				return;
-			}
+			if (fb == null)
+				return;
 
-			if (getArrow.contains(aw)) {
+			if (getFireball.contains(fb)) {
 
-				getArrow.remove(aw);
-				Location location = aw.getLocation();
-				Squid squid = getSquid.get(aw);
+				getFireball.remove(fb);
+				Location location = fb.getLocation();
+				Squid squid = getSquid.get(fb);
 
-				playerEffect(aw);
+				playerEffect(fb);
 				squid.remove();
-				aw.remove();
 
-				aw.getWorld().playSound(aw.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 2, 2);
+				fb.getWorld().playSound(fb.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 2, 2);
 
 				ExplodeEffect expef = new ExplodeEffect(Skywars.effectmanager);
 				expef.visibleRange = 300;
-				expef.setLocation(aw.getLocation());
+				expef.setLocation(fb.getLocation());
 				expef.start();
 
 				location.getWorld().playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 4, 10);
@@ -134,6 +145,35 @@ public class Kraken implements Listener, ItemType {
 
 					b.setType(Material.COAL_BLOCK);
 					coalBlock.add(b);
+					clayBlock.add(b);
+
+				}
+				
+				Collection<Entity> entities = location.getWorld().getNearbyEntities(location, 3, 3, 3);
+				for (Entity entity : entities) {
+
+					if (entity.getType() == EntityType.PLAYER) {
+						Player p = (Player) entity;
+
+						if ((SoloPlayerManager.getPlayersInGameList().contains(getPlayer.get(fb))
+								|| TeamPlayerManager.getPlayersInGameList().contains(getPlayer.get(fb)))) {
+
+							if (Skywars.currentMatchType == SkywarsType.SW_NORMAL_TEAMS
+									|| Skywars.currentMatchType == SkywarsType.SW_INSANE_TEAMS
+									|| Skywars.currentMatchType == SkywarsType.SW_Z_TEAMS) {
+
+								TeamPlayerBattleListener.makeZDamage(getPlayer.get(fb), getPlayer.get(fb), 5,
+										net.omniblock.skywars.games.teams.events.TeamPlayerBattleListener.DamageCauseZ.THORA);
+								continue;
+
+							}
+
+							SoloPlayerBattleListener.makeZDamage(p, getPlayer.get(fb), 5, DamageCauseZ.THORA);
+					
+							continue;
+
+						}
+					}
 
 				}
 
@@ -149,18 +189,16 @@ public class Kraken implements Listener, ItemType {
 		Player player = event.getPlayer();
 
 		if (SoloPlayerManager.getPlayersInGameList().contains(player) && player.getGameMode() == GameMode.SURVIVAL) {
-			if (player.getLocation().add(0, -1, 0).getBlock().getType() == Material.COAL_BLOCK) {
+			if (player.getLocation().add(0, -1, 0).getBlock().getType() == Material.COAL_BLOCK
+					|| player.getLocation().add(0, -1, 0).getBlock().getType() == Material.CLAY) {
 
 				Block block = event.getPlayer().getLocation().add(0, -1, 0).getBlock();
-				List<Block> clayBlock = new ArrayList<Block>();
+				
 
 				if (coalBlock.contains(block)) {
 
 					block.setType(Material.CLAY);
-					Block clay_block = block;
-					clayBlock.add(clay_block);
-
-					block.getLocation().getWorld().playEffect(block.getLocation(), Effect.SMOKE, 20);
+					
 					event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 6, 2));
 
 					coalBlock.remove(block);
@@ -171,30 +209,38 @@ public class Kraken implements Listener, ItemType {
 
 				if (clayBlock.contains(block)) {
 
-					block.setType(Material.QUARTZ);
-					event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 6, 2));
 					clayBlock.remove(block);
+					event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 6, 2));
+					
+					new BukkitRunnable() {
 
-					block.getLocation().getWorld().playEffect(block.getLocation(), Effect.SMOKE, 20);
-
+						@Override
+						public void run() {
+							
+							block.setType(Material.AIR);
+							player.playSound(player.getLocation(), Sound.BLOCK_DISPENSER_LAUNCH, 6, 10);
+						
+						}
+						
+					}.runTaskLater(Skywars.getInstance(), 20L);
+					
 					return;
 
 				}
-
 			}
 		}
 	}
 
 	public void playerEffect(Entity et) {
 
-		for (Entity entity : getSquid.get(et).getNearbyEntities(15, 15, 15)) {
+		for (Entity entity : getSquid.get(et).getNearbyEntities(8, 8, 8)) {
 
 			if (entity == null) {
 				continue;
 			}
 
 			if (entity instanceof Player) {
-				
+
 				Player p = (Player) entity;
 				p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 6, 2));
 
@@ -205,6 +251,5 @@ public class Kraken implements Listener, ItemType {
 			}
 
 		}
-
 	}
 }
--- src/net/omniblock/skywars/patch/managers/chest/defaults/events/Potion.java
+++ src/net/omniblock/skywars/patch/managers/chest/defaults/events/Potion.java
@@ -14,6 +14,7 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.omniblock.skywars.Skywars;
+import net.omniblock.skywars.SkywarsGameState;
 import net.omniblock.skywars.games.solo.managers.SoloPlayerManager;
 import net.omniblock.skywars.games.teams.managers.TeamPlayerManager;
 import net.omniblock.skywars.patch.managers.chest.defaults.events.type.ItemType;
@@ -25,52 +26,47 @@ public class Potion implements ItemType, Listener {
 	@EventHandler
 	public void HealPot(PlayerInteractEvent event) {
 
-		Player player = event.getPlayer();
+		if (Skywars.getGameState() != SkywarsGameState.IN_GAME) return;
 
-		if (SoloPlayerManager.getPlayersInGameList().contains(player)
-				|| TeamPlayerManager.getPlayersInGameList().contains(player)
-						&& player.getGameMode() == GameMode.SURVIVAL) {
+		if (SoloPlayerManager.getPlayersInGameList().contains(event.getPlayer())
+				|| TeamPlayerManager.getPlayersInGameList().contains(event.getPlayer())) {
+			
+			if(event.getPlayer().getItemInHand().getType() == Material.POTION) {
+				
 
-			if (event.getPlayer().getItemInHand().hasItemMeta()) {
-				if (event.getPlayer().getItemInHand().getItemMeta().hasDisplayName()) {
-					if (event.getPlayer().getItemInHand().getType() == Material.POTION) {
+				if (event.getAction() == Action.LEFT_CLICK_BLOCK
+						|| event.getAction() == Action.RIGHT_CLICK_BLOCK
+						|| event.getAction() == Action.RIGHT_CLICK_AIR
+						|| event.getAction() == Action.LEFT_CLICK_AIR) {
 
-						if (event.getAction() == Action.LEFT_CLICK_BLOCK
-								|| event.getAction() == Action.RIGHT_CLICK_BLOCK
-								|| event.getAction() == Action.RIGHT_CLICK_AIR
-								|| event.getAction() == Action.LEFT_CLICK_AIR) {
+					if (event.getClickedBlock() != null) {
+						if (event.getClickedBlock().getType() == Material.CHEST
+								|| event.getClickedBlock().getType() == Material.TRAPPED_CHEST
+								|| event.getClickedBlock().getType() == Material.JUKEBOX) {
 
-							if (event.getClickedBlock() != null) {
-								if (event.getClickedBlock().getType() == Material.CHEST
-										|| event.getClickedBlock().getType() == Material.TRAPPED_CHEST
-										|| event.getClickedBlock().getType() == Material.JUKEBOX) {
-
-									event.setCancelled(true);
-									return;
-
-								}
-							}
-
-							for (PotionEffect potion : player.getActivePotionEffects()) {
-								player.removePotionEffect(potion.getType());
-							}
-
-							player.getInventory().setItemInHand(null);
-							player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_DRINK, 3, -30);
-							player.playSound(player.getLocation(), Sound.ENTITY_BAT_TAKEOFF, 3, -30);
-							player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 2, -30);
-							player.setHealth(player.getMaxHealth());
-							player.setHealth((int) 4);
-							player.setFoodLevel((int) 5);
-
-							player.playEffect(EntityEffect.VILLAGER_HAPPY);
-							potionEffect(player);
+							event.setCancelled(true);
+							return;
+							
+							
 
 						}
 					}
+				
+					for (PotionEffect potion : event.getPlayer().getActivePotionEffects()) {
+						event.getPlayer().removePotionEffect(potion.getType());
+					}
+
+					event.getPlayer().getInventory().setItemInHand(null);
+					event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_GENERIC_DRINK, 3, -30);
+					event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_BAT_TAKEOFF, 3, -30);
+					event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_VILLAGER_YES, 2, -30);
+					event.getPlayer().setFoodLevel((int) 1);
+
+					event.getPlayer().playEffect(EntityEffect.VILLAGER_HAPPY);
+					potionEffect(event.getPlayer());
 				}
 			}
-		}
+		}			
 	}
 
 	public void potionEffect(Player player) {
@@ -83,16 +79,23 @@ public class Potion implements ItemType, Listener {
 			public void run() {
 				if (TIME != 40) {
 					TIME += 20;
+					player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 5, 10));
 					player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 5, 10));
 					player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 5, 10));
 					player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 5, 5));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 80, 10));
 					player.getWorld().playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 5, 5);
 
 				} else {
-					player.getWorld().playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 2, 2);
-					player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 40, 10));
-					player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 40, 10));
-					player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 80, 2));
+					player.removePotionEffect(PotionEffectType.LEVITATION);
+					player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ARROW_SHOOT, 7, 10);
+					player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 160, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 160, 15));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 160, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 160, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 160, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 160, 10));
+					player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 160, 2));
 					player.setHealth(player.getMaxHealth());
 					player.setFoodLevel((int) 20);
 					cancel();
